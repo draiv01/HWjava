225bc7267640fae94af471eb7e242049a6cda905
Alexandr Makarov <makarena2009@yandex.ru>
2023-05-01 18:23:34 +0300

    homework04

---
+++ HW4.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class HW4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<String> list = new LinkedList<>();
+
+        while (true) {
+            System.out.println("Введите print/revert: ");
+            String input = scanner.nextLine();
+
+            if (input.equals("print")) {
+                System.out.println("Строки в обратном порядке:");
+                while (!list.isEmpty()) {
+                    System.out.println(list.removeLast());
+                }
+            } else if (input.equals("revert")) {
+                if (!list.isEmpty()) {
+                    list.removeLast();
+                    System.out.println("строка удалена");
+                } else {
+                    System.out.println("ошибка");
+                }
+            } else {
+                list.addLast(input);
+            }
+        }
+    }
+}
